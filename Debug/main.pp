051fc8d55d59e2300e99e75d60a92a574b8e9c72
einballimwasser <einballimwasser@web.de>
2013-12-11 14:27:08 +0100

    Initial commit

---
+++ Debug/main.pp
@@ -0,0 +1,113 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../main.h
+main.obj: C:/ti/C6xCSL/include/csl.h
+main.obj: C:/ti/C6xCSL/include/csl_chip.h
+main.obj: C:/ti/C6xCSL/include/csl_stdinc.h
+main.obj: C:/ti/C6xCSL/include/csl_stdinchal.h
+main.obj: C:/ti/C6xCSL/include/tistdtypes.h
+main.obj: C:/ti/C6xCSL/include/csl_chiphal.h
+main.obj: C:/ti/C6xCSL/include/csl_emifhal.h
+main.obj: C:/ti/C6xCSL/include/csl_chip.h
+main.obj: C:/ti/C6xCSL/include/csl_irq.h
+main.obj: C:/ti/C6xCSL/include/csl_irqhal.h
+main.obj: C:/ti/C6xCSL/include/csl_timer.h
+main.obj: C:/ti/C6xCSL/include/csl_timerhal.h
+main.obj: C:/ti/DSK6713/c6000/dsk6713/include/dsk6713.h
+main.obj: C:/ti/DSK6713/c6000/dsk6713/include/dsk6713_led.h
+main.obj: C:/ti/DSK6713/c6000/dsk6713/include/dsk6713_dip.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/std.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/tistdtypes.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdint.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/swi.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/fxn.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/sts.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/hwi.h
+main.obj: C:/ti/C6xCSL/include/csl_edma.h
+main.obj: C:/ti/C6xCSL/include/csl_edmahal.h
+main.obj: C:/ti/C6xCSL/include/csl_mcbsp.h
+main.obj: C:/ti/C6xCSL/include/csl_mcbsphal.h
+main.obj: C:/Users/einball/TI_Projekte/LED_blink/Debug/LED_blinkcfg.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/prd.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/hst.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/pip.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/tsk.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/knl.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/atm.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/que.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/mem.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/sem.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/obj.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/sys.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/trg.h
+main.obj: C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/_hook.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/log.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/_log.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/trc.h
+main.obj: ../config_AIC23.h
+main.obj: ../config_MCBSP.h
+
+../main.c: 
+../main.h: 
+C:/ti/C6xCSL/include/csl.h: 
+C:/ti/C6xCSL/include/csl_chip.h: 
+C:/ti/C6xCSL/include/csl_stdinc.h: 
+C:/ti/C6xCSL/include/csl_stdinchal.h: 
+C:/ti/C6xCSL/include/tistdtypes.h: 
+C:/ti/C6xCSL/include/csl_chiphal.h: 
+C:/ti/C6xCSL/include/csl_emifhal.h: 
+C:/ti/C6xCSL/include/csl_chip.h: 
+C:/ti/C6xCSL/include/csl_irq.h: 
+C:/ti/C6xCSL/include/csl_irqhal.h: 
+C:/ti/C6xCSL/include/csl_timer.h: 
+C:/ti/C6xCSL/include/csl_timerhal.h: 
+C:/ti/DSK6713/c6000/dsk6713/include/dsk6713.h: 
+C:/ti/DSK6713/c6000/dsk6713/include/dsk6713_led.h: 
+C:/ti/DSK6713/c6000/dsk6713/include/dsk6713_dip.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/std.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/tistdtypes.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdint.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/swi.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/fxn.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/sts.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/hwi.h: 
+C:/ti/C6xCSL/include/csl_edma.h: 
+C:/ti/C6xCSL/include/csl_edmahal.h: 
+C:/ti/C6xCSL/include/csl_mcbsp.h: 
+C:/ti/C6xCSL/include/csl_mcbsphal.h: 
+C:/Users/einball/TI_Projekte/LED_blink/Debug/LED_blinkcfg.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/prd.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/hst.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/pip.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/tsk.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/knl.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/atm.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/que.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/mem.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/sem.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/obj.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/sys.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stdarg.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/trg.h: 
+C:/ti/ccsv5/tools/compiler/c6000_7.4.4/include/stddef.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/_hook.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/log.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/_log.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/trc.h: 
+../config_AIC23.h: 
+../config_MCBSP.h: 
