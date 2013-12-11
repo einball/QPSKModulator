99e924b60d4067f30465f6bf558cf493381e9c81
einballimwasser <einballimwasser@web.de>
2013-12-11 14:45:19 +0100

    Initial fixed commit I hope ...

---
+++ config_AIC23.h
@@ -0,0 +1,20 @@
+/*
+ * config_AIC23.h
+ *
+ *  Created on: 20.10.2013
+ *      Author: einball
+ */
+
+#ifndef CONFIG_AIC23_H_
+#define CONFIG_AIC23_H_
+
+#include <stdint.h>
+#include <csl.h>
+#include <csl_mcbsp.h>
+
+
+void conf_AIC23();
+void setReg_AIC23(MCBSP_Handle hMcbsp,unsigned short regnum, unsigned short regval);
+
+
+#endif /* CONFIG_AIC23_H_ */
